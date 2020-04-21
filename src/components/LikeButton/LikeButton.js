d781c12dc83c9033cb781be7991fd9c875abd37a
Lorenzo Spinelli <lorenzo.spinelli@register.it>
2020-04-21 09:10:21 +0200

    refactor: remove clap limit

--- src/components/LikeButton/LikeButton.js
+++ src/components/LikeButton/LikeButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import "./LikeButton.css"
 import LikeImage from "./LikeButton.svg"
 import { Fab } from "@material-ui/core"
@@ -13,20 +13,17 @@ const ADD_LIKE = gql`
 
 export default function LikeButton({ postId, likes }) {
   const [clapCount, seClapCount] = useState(likes)
-  const [myClaps, setMyClaps] = useState(0)
 
   const [addLike] = useMutation(ADD_LIKE)
 
   const onClapHanlder = () => {
     seClapCount(clapCount + 1)
-    setMyClaps(myClaps + 1)
     addLike({ variables: { postId } })
   }
 
   return (
     <>
       <Fab
-        disabled={myClaps === 50}
         size="small"
         className="like-button"
         aria-label="Clap if you liked this article"
