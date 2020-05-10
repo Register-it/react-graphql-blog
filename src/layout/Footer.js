5c1f05031507273074ec5525cfc21faca6e40703
Spyna <lscaper@gmail.com>
2020-05-10 14:43:03 +0200

    style: add link to home in footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles"
 import Container from "@material-ui/core/Container"
 import Typography from "@material-ui/core/Typography"
 import Link from "@material-ui/core/Link"
+import { Link as InternalLink} from "react-router-dom"
 
 function Copyright() {
   return (
@@ -31,7 +32,7 @@ export default function Footer(props) {
     <footer className={classes.footer}>
       <Container maxWidth="lg">
         <Typography variant="h6" align="center" gutterBottom>
-          GraphQL Demo Blog
+          <InternalLink to="/">GraphQL Demo Blog</InternalLink>
         </Typography>
         <Typography
           variant="subtitle1"
