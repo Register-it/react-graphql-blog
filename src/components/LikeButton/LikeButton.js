1da1d50ab3b6e7e331610a3d7305a38aa1372e5d
Spyna <lscaper@gmail.com>
2020-07-04 17:54:31 +0200

    fix: eslint

--- src/components/LikeButton/LikeButton.js
+++ src/components/LikeButton/LikeButton.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 import "./LikeButton.css"
 import LikeImage from "./LikeButton.svg"
 import { useMutation } from "@apollo/react-hooks"
